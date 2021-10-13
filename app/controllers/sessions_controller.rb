0a581ca2c5507e5126d5951a38ef707a149e6a33
Rguardado1009 <rguardado1009@gmail.com>
2021-10-13 18:18:08 -0400

    10.13.21 daily push

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
     def create
       user = User.find_by_username(params[:username])
-      if user&.authenticate(params[:password])
+      if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         render json: user, status: :ok
       else
